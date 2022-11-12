e73af13107aad2fa74d17c7914bc7ce24ae64856
miao-tianjia <tianjiaAM@gmail.com>
2022-11-12 13:14:20 +0800

    firebase update

--- src/uifire.js
+++ src/uifire.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/compat/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyATUsMADB8w26V5f3IRRgpsVsOLny8H82Q",
-    authDomain: "group-project-32538.firebaseapp.com",
-    projectId: "group-project-32538",
-    storageBucket: "group-project-32538.appspot.com",
-    messagingSenderId: "1094587824694",
-    appId: "1:1094587824694:web:960ba8687181c0c9043697"
-  };
+  apiKey: "AIzaSyCUDVuRuqdeUOr7h2JpxynS_cxdII3yjiA",
+  authDomain: "bt3103-group-project.firebaseapp.com",
+  projectId: "bt3103-group-project",
+  storageBucket: "bt3103-group-project.appspot.com",
+  messagingSenderId: "924520970735",
+  appId: "1:924520970735:web:5268e0e61be079cca91e1a"
+};
 
 firebase.initializeApp(firebaseConfig)
 
