f4513b2cd646d6daf837d30228735c14860b8e80
XiHuijia <1348045797@qq.com>
2022-11-12 15:20:02 +0800

    update style

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        <h2>
+        
         <div>
             <div class="item">Grow with Us</div>
             <div class="item">Business with Us</div>
@@ -17,19 +17,14 @@
             <div class="item">Sponsored Placements</div>
             <div class="item">Exhibitions</div>          
         </div>
-
-        </h2>
-
-        <div>        
-        <div class="title">
-            <h2>Photographic Society of South East Asia</h2>
-        </div>
+        <div class="title">Photographic Society of South East Asia</div>
         <div class="logo-list">
-            <img @click="jump3page('https://twitter.com')" src="../assets/image/01ab6619093f45388d66736ec22e5885.png" alt="">
-            <img @click="jump3page('http://www.facebook.com')" src="../assets/image/23fd2a2be53141ed810f4d3dcdcd01fa.png" alt="">
-            <img @click="jump3page('https://www.instagram.com')" src="../assets/image/81af6121f84c41a5b4391d7d37fce12a.png" alt="">
+            <a href="https://twitter.com"><img src="../assets/image/01ab6619093f45388d66736ec22e5885.png" alt="Twitter" /></a>
+            <a href="http://www.facebook.com"><img src="../assets/image/23fd2a2be53141ed810f4d3dcdcd01fa.png" alt="Facebook" /></a>
+            <a href="https://www.instagram.com"><img src="../assets/image/81af6121f84c41a5b4391d7d37fce12a.png" alt="Instagram" /></a>
         </div>
-        </div>
+
+
 
     </footer>
 </template>
@@ -52,13 +47,15 @@ footer {
   font-family: 'Noto Nastaliq Urdu', serif;
 
   letter-spacing: 0.2em;
-  h2 {
+  
   > div {
     display: flex;
     padding-bottom: 5px;
     &.second{
       > div.item {
+        font-family: 'Noto Nastaliq Urdu', serif;
         font-size: 15px !important;
+        font-weight: 1;
         color: #fff !important;
       }
     }
@@ -66,36 +63,59 @@ footer {
       flex-shrink: 0;
       margin-right: 100px;
       width: 287px;
-      font-size: 20px;
-      color: #fff !important;
       cursor: pointer;
+      --fill-color: #fff;
+      position: relative;
+      display: block;
+      padding: 4px 0;
+      font: 10 3rem Merienda;
+      text-decoration: none;
+      text-transform: uppercase;
+      -webkit-text-stroke: 2px var(--fill-color);
+      background: linear-gradient(var(--fill-color) 0 100%) left / 0 no-repeat;
+      color: transparent;
+      background-clip: text;
+      transition: 0.5s linear;
       &:hover {
-        color: #000 !important;
+        background-size:100%;
       }
     }
   }
-  }
-
-  .title {
-    font-family: 'Kolker Brush', cursive;
-  }
-
-  h2 {
-    font-size: 55px;
-    letter-spacing: 0.2em;
-    color: #fff;
-  }
+  
   .logo-list {
-    margin-left: 40px;
+    margin: 40px 0 40px 430px;
+    align-content: center;
     img {
-      
-      height: 30px;
-      width: 30px;
-      margin-right: 3px;
+      height: 40px;
+      width: 40px;
+      margin-right: 5px;
     };
     &:hover {
     color: #000 !important;
     cursor: pointer;
+    }
+  }
+  .title {
+    margin-top: 60px;
+    font-family: 'Kolker Brush', cursive;
+    color: #f8cce7;
+    font-size: 5rem;
+    text-transform: uppercase;
+    animation: shining 0.1s alternate infinite;
+  }
+
+  @keyframes shining {
+    from {
+      text-shadow: 0 0 6px rgba(182, 211, 207, 0.9),
+        0 0 30px rgba(206, 182, 211, 0.3), 0 0 12px rgba(202, 15, 223, 0.5),
+        0 0 21px rgba(223, 15, 123, 0.9), 0 0 34px rgba(223, 15, 116, 0.8),
+        0 0 54px rgba(223, 98, 15, 0.9);
+    }
+    to {
+      text-shadow: 0 0 6px rgba(182, 211, 207, 1),
+        0 0 30px rgba(211, 192, 182, 0.4), 0 0 12px rgba(223, 15, 60, 0.6),
+        0 0 22px rgba(223, 123, 15, 0.8), 0 0 38px rgba(223, 15, 126, 0.9),
+        0 0 60px rgb(223, 60, 15);
     }
   }
 }
