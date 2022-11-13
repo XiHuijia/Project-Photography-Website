a1dc818163053420c1520de37774cef359e62025
miao-tianjia <tianjiaAM@gmail.com>
2022-11-13 15:34:53 +0800

    user profile page

---
+++ src/view/UserProfile.vue
@@ -0,0 +1,22 @@
+<template>
+    <HeadLine/>
+    <div class="container">
+        <div id="username">{{this.user.displayName}}</div>
+        <div id="following">{{this.user.following}}</div>
+        <button class="jumppage" @click="this.$router.push(MyPortfolio)">My Portfolio</button>
+        <button class="jumppage" @click="this.$router.push(FollowingPage)">Following</button>
+    </div>
+    <MyFooter/>
+</template>
+
+<script>
+import HeadLine from '@/components/HeadLine.vue';
+import MyFooter from '@/components/MyFooter.vue';
+
+export default {
+    components:{
+        HeadLine,
+        MyFooter
+    }
+}
+</script>
